498b8ce7a4cb411bee5f6448a01cc0971e3e670b
Avnish Kumar <avikeid2007@live.com>
2019-11-30 20:51:02 +0530

    Add project files.

---
+++ MyConsoleApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyConsoleApp
+{
+    class Program
+    {
+      
+        static void Main(string[] args)
+        {
+            ServiceReference1.Service1Client sclient = new ServiceReference1.Service1Client();
+           int res= sclient.Sum(4,6);
+            Console.WriteLine("Sum =" + res);
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ WindowsServiceForWCF/Service1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;    
+using WcfService;    //WcfService Reference
+
+namespace WindowsServiceForWCF
+{
+    public partial class Service1 : ServiceBase
+    {
+        internal static ServiceHost WcfServiceHost = null;   //Create a Service Host
+
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)   //Run While WindowServices Start 
+        {
+
+            if (WcfServiceHost != null)
+            {
+                WcfServiceHost.Close();
+            }
+            WcfServiceHost = new ServiceHost(typeof(WcfService.Service1));  //Run While WindowServices Stop 
+
+            WcfServiceHost.Open();
+        }
+
+        protected override void OnStop()
+        {
+            if (WcfServiceHost != null)
+            {
+                WcfServiceHost.Close();
+                WcfServiceHost = null;
+            }
+        }
+    }
+}
